15a4070466ef0e008ec2c8004f33096bae51415e
Oryfox <56044405+Oryfox@users.noreply.github.com>
2022-06-15 23:02:19 +0200

    Once again reworked navbar buttons and logo

--- frontend/src/views/NavigationView.vue
+++ frontend/src/views/NavigationView.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <img src="/icon.png" alt="Logo" />
+    <img src="/icon.png" alt="Logo" @click="this.$router.push('/about')"/>
     <!--<RouterLink to="/" class="rl">Home</RouterLink>
     <RouterLink to="/about" class="rl">About</RouterLink>-->
     <div v-if="cookie" class="navb">
@@ -31,8 +31,8 @@
           Back
         </div>
 
-        <RouterLink to="/records" class="rl">Records</RouterLink>
-        <RouterLink to="/artists" class="rl">Artists</RouterLink>
+        <RouterLink to="/records" class="rl underline">Records</RouterLink>
+        <RouterLink to="/artists" class="rl underline">Artists</RouterLink>
       </div>
 
       <div class="navbar-buttons">
@@ -56,7 +56,7 @@
         </div>
 
         <div class="navbar-button" @click="this.$router.push('/logout')">
-          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-circle user-button" viewBox="0 0 16 16">
             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
           </svg>
@@ -118,10 +118,6 @@ nav {
   justify-content: center;
   transition-duration: 0.3s;
 }
-.navbar-button:hover {
-  background: white;
-  transition-duration: 0.3s;
-}
 .create-button {
   transform: rotate(0deg) scale(1, 1);
   transition: transform 400ms;
@@ -130,6 +126,14 @@ nav {
 .navbar-button:hover .create-button {
   transform: rotate(90deg) scale(1.2, 1.2);
 }
+.user-button {
+  transform: scale(1, 1);
+  transition: transform 400ms;
+  cursor: pointer;
+}
+.navbar-button:hover .user-button {
+  transform: scale(1.2, 1.2);
+}
 
 .router-links {
   display: flex;
@@ -137,7 +141,6 @@ nav {
 .rl {
   color: var(--text-color);
   text-decoration: none;
-  border-radius: 1rem;
   margin-right: 1.4rem;
   transition-duration: 0.3s;
   display: flex;
@@ -147,20 +150,42 @@ nav {
   cursor: pointer;
   stroke: var(--text-color);
 }
-.rl::after  {
-  display: inline-block;
+/*.rl::after  {*/
+/*  display: inline-block;*/
+/*  content: "";*/
+/*  position: absolute;*/
+/*  width: 2px;*/
+/*  height: 20px;*/
+/*  right: -0.7rem;*/
+/*  border-right: 2px solid var(--text-color);*/
+/*}*/
+/*.rl:last-child {*/
+/*  padding-right: 0;*/
+/*}*/
+/*.rl:last-child::after {*/
+/*  display: none;*/
+/*}*/
+.underline:hover::after {
+  transform: translate3d(0, 0, 0);
+  opacity: 1;
+}
+.underline {
+  transition-duration: 0.5s;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+}
+.underline:after {
   content: "";
   position: absolute;
-  width: 2px;
-  height: 20px;
-  right: -0.7rem;
-  border-right: 2px solid var(--text-color);
-}
-.rl:last-child {
-  padding-right: 0;
-}
-.rl:last-child::after {
-  display: none;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 0.1em;
+  background-color: var(--primary-color);
+  opacity: 0;
+  transition: opacity 300ms, transform 300ms;
+  transform: translate3d(-100%, 0, 0);
 }
 
 img {
@@ -168,5 +193,12 @@ img {
   height: 80%;
   margin-right: 2rem;
   margin-left: 0.5rem;
+  transform: rotate(0deg);
+  transition: transform 400ms;
+  cursor: pointer;
+}
+img:hover {
+  transform: rotate(-45deg);
+  transition: transform 400ms;
 }
 </style>
